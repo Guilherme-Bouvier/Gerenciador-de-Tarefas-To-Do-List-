c38d8795d64fbca244b6dc3dc5c140cd18ca0233
GUILHERME BOUVIER <guilhermebouva@gmail.com>
2026-04-16 21:44:01 -0300

    Remove commented-out instructions in tarefas.py

--- tarefas.py
+++ tarefas.py
@@ -31,20 +31,3 @@ def concluir_tarefa(indice):
         print("Tarefa concluída com sucesso!")
     except IndexError:
         print("Índice inválido. Tente novamente.")
-
-
-
-
-
-#
-
-# 1. A Biblioteca: No topo do tarefas.py, importe a biblioteca json.
-
-# 2. Salvando: Crie a função salvar_dados(). Use with open("dados.json", "w") e json.dump() para gravar a lista
-# inteira.
-
-# 3. Carregando: Crie a função carregar_dados(). Use o modo leitura ("r") e json.load() com um try/except
-# FileNotFoundError.
-
-# 4. A Integração: Chame carregar_dados() no início do main.py, e chame salvar_dados() toda vez que uma tarefa
-# for adicionada ou concluída!
