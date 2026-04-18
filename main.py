dff7493e6b6cb175e1656dd425a51879720c26ab
unknown <guilhermebouva@gmail.com>
2026-04-17 22:49:48 -0300

    Ajuste 2

--- main.py
+++ main.py
@@ -1,37 +1,72 @@
-from tarefas import adicionar_tarefa, listar_tarefas, concluir_tarefa
+from tarefas import carregar_dados, adicionar_tarefa, listar_tarefas, concluir_tarefa, remover_tarefa, limpar_tela
 
+import time
+
+tempo = 2
+
+carregar_dados()
 
 while True:
-    print("\n=== GERENCIADOR DE TAREFAS ===")
+    limpar_tela()
+
+    print("\n=== GERENCIADOR DE TAREFAS ===\n")
     print("1 - Adicionar tarefa")
     print("2 - Listar tarefas")
     print("3 - Concluir tarefa")
-    print("4 - Sair")
+    print("4 - Remover tarefa")
+    print("5 - Sair")
 
     opcao = input("Escolha uma opção: ")
 
     if opcao == "1":
+        limpar_tela()
+        print("===========LISTA DE TAREFAS==========\n")
         descricao = input("Digite a descrição da tarefa: ")
         adicionar_tarefa(descricao)
-        
+        time.sleep(tempo)
 
     elif opcao == "2":
+        limpar_tela()
+        print("===========LISTA DE TAREFAS==========\n")
         listar_tarefas()
-        
+        time.sleep(tempo)
 
     elif opcao == "3":
+        limpar_tela()
+        print("===========TAREFAS CONCLUÍDAS==========\n")
         listar_tarefas()
+        
         try:
-            indice = int(input("Digite o número da tarefa: "))
+            indice = int(input("\nDigite o número da tarefa: "))
             concluir_tarefa(indice)
+            print("Tarefa concluída com sucesso!\n")
+            listar_tarefas()
+
         except ValueError:
             print("Digite um número válido.")
-        
-
+        time.sleep(tempo)
 
     elif opcao == "4":
+        limpar_tela()
+        print("===========REMOVER TAREFAS==========\n")
+        listar_tarefas()
+
+        try:
+            indice = int(input("\nDigite o número da tarefa para remover: "))
+            remover_tarefa(indice)
+            print("Tarefa removida com sucesso!\n")
+                        
+        except ValueError:
+            print("Digite um número válido.")
+
+        time.sleep(tempo)
+
+    elif opcao == "5":
+        limpar_tela()
         print("Encerrando o programa...")
         break
-
+        
     else:
         print("Opção inválida.")
+        limpar_tela()
+        time.sleep(tempo)
--- tarefas.py
+++ tarefas.py
@@ -1,33 +1,76 @@
 import json
-
+import os
 lista_tarefas = []
 
 
 
 def adicionar_tarefa(descricao):
+    descricao = descricao.strip()
+    
+    for t in lista_tarefas:
+        if t["descricao"].lower() == descricao.lower():
+            print("Já existe uma tarefa com esse nome!")
+            return
+    
     tarefa = {
         "descricao": descricao,
         "concluida": False
     }
+    
     lista_tarefas.append(tarefa)
-
+    salvando_arquivo()
+    print("Tarefa criada com sucesso!")
 
 def listar_tarefas():
     if not lista_tarefas:
         print("Nenhuma tarefa cadastrada.")
-      
+        return
 
-    for tarefa in lista_tarefas:
-        print(f"- {tarefa['descricao']}")
-        
-        for i, tarefa in enumerate(lista_tarefas):
-            status = "[X]" if tarefa["concluida"] else "[ ]"
-            print(f"{i} - {status} {tarefa['descricao']}")
-        
+    for i in range(len(lista_tarefas)):
+        tarefa = lista_tarefas[i]
+        status = "[X]" if tarefa["concluida"] else "[ ]"
+        print(f"{i} - {status} {tarefa['descricao']}")
+
 
 def concluir_tarefa(indice):
     try:
         lista_tarefas[indice]["concluida"] = True
-        print("Tarefa concluída com sucesso!")
+        salvando_arquivo()
+
     except IndexError:
         print("Índice inválido. Tente novamente.")
+
+def remover_tarefa(indice):
+    try:
+        lista_tarefas.pop(indice)
+        salvando_arquivo()
+        
+
+    except IndexError:
+        print("Índice inválido.")
+
+
+def salvando_arquivo ():
+    try:
+        with open("tarefas.json", "w") as arquivo:
+            json.dump(lista_tarefas, arquivo,)
+
+    except Exception as e:
+        print(f"Erro ao salvar tarefas: {e}")
+    
+def carregar_dados():
+    global lista_tarefas
+    try:
+        with open("tarefas.json", "r") as arquivo:
+            lista_tarefas = json.load(arquivo)
+
+    except FileNotFoundError:
+        lista_tarefas = []
+        print("Arquivo não encontrado. Criando lista vazia.")
+
+    except Exception as e:
+        lista_tarefas = []
+        print(f"Erro ao ler tarefas: {e}")
+
+def limpar_tela():
+    os.system("cls")
